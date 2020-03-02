f3c040d4c11afa6c68b82ac273628894ca79d395
tylercchase <tylercchase@gmail.com>
2020-03-02 13:33:10 -0900

    Adjust size of window

--- fishinGame/main.cpp
+++ fishinGame/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 400), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1000, 600), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
     Fish f{};
