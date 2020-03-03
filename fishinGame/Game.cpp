a55d0a9e2fe985479bc82300d1f5a191682c7225
tylercchase <tylercchase@gmail.com>
2020-03-02 20:27:54 -0900

    Check for movement off screen

--- fishinGame/Game.cpp
+++ fishinGame/Game.cpp
@@ -34,45 +34,74 @@ void Game::processKeys(){
         //Right
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         {
-            fish.sprite.move(.1,0);
+            if ( fish.sprite.getPosition().x + 50 >= window.getSize().x ) { }
+            else {
+                fish.sprite.move(.1,0);
+            }
+
             fish.sprite.setTextureRect(sf::IntRect(300, 0, -300, 300));;
 
         }
         //Left
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-            fish.sprite.move(-0.1,0);
+            if ( fish.sprite.getPosition().x <= 0 ) {
+            }else{
+                fish.sprite.move(-0.1,0);
+            }
             fish.sprite.setTextureRect(sf::IntRect(0, 0, 300, 300));;
-
         }
         //Down
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-            fish.sprite.move(0,0.1);
+            if ( fish.sprite.getPosition().y + 50 >= window.getSize().y ) { }
+            else {
+                fish.sprite.move(0,0.1);
+            }
         }
         //Up
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
-            fish.sprite.move(0,-0.1);
+            if ( fish.sprite.getPosition().y <= 0 ) {
+
+            }
+            else {
+                fish.sprite.move(0,-0.1);
+            }
+
         }
         //Fisherman Movement
-        //Right
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::L))
         {
-            fisherman.sprite.move(.1,0);
+            if ( fisherman.sprite.getPosition().x + 50 >= window.getSize().x ) { }
+            else {
+                fisherman.sprite.move(.1,0);
+            }
+
             fisherman.sprite.setTextureRect(sf::IntRect(300, 0, -300, 300));;
 
         }
         //Left
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::J)){
-            fisherman.sprite.move(-0.1,0);
+            if ( fisherman.sprite.getPosition().x <= 0 ) {
+            }else{
+                fisherman.sprite.move(-0.1,0);
+            }
             fisherman.sprite.setTextureRect(sf::IntRect(0, 0, 300, 300));;
-
         }
         //Down
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::K)){
-            fisherman.sprite.move(0,0.1);
+            if ( fisherman.sprite.getPosition().y + 50 >= window.getSize().y ) { }
+            else {
+                fisherman.sprite.move(0,0.1);
+            }
         }
         //Up
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::I)){
-            fisherman.sprite.move(0,-0.1);
+            if ( fisherman.sprite.getPosition().y <= 0 ) {
+
+            }
+            else {
+                fisherman.sprite.move(0,-0.1);
+            }
+
         }
 }
 void Game::update(){
