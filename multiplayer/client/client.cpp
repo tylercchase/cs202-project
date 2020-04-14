4baa0a187712ef80bd71b282b8282ac86c6062b3
tylercchase <tylercchase@gmail.com>
2020-04-14 02:03:17 -0800

    Use correct IP for client

--- multiplayer/client/client.cpp
+++ multiplayer/client/client.cpp
@@ -3,7 +3,7 @@
 int main(){
     char data[100] = "hello world!";
     sf::TcpSocket socket;
-    sf::Socket::Status status = socket.connect("10.244.2.58", 42069);
+    sf::Socket::Status status = socket.connect("134.209.140.123", 42069);
     if (status != sf::Socket::Done)
     {
         std::cout << "Can't connect" << std::endl;
@@ -11,11 +11,12 @@ int main(){
     }
     char data1[100];
     std::size_t received;
-
+std::cout << "VCD" << std::endl;
     // TCP socket:
     if (socket.send(data, 100) != sf::Socket::Done)
     {
         // error...
         std::cout << "Couldn't send" << std::endl;
     }
+    std::cout << "ASD" ;
 }
